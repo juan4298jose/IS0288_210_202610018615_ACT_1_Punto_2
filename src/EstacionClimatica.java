9effdf2082082c0eede6c8880105a89ffefea68b
juan4298jose <juanjosediazlop8@gmail.com>
2026-05-29 02:49:11 +0000

    primera vercion

---
+++ src/EstacionClimatica.java
@@ -0,0 +1,19 @@
+public class EstacionClimatica {
+    public static void main(String[] arg){
+
+double [] tempertura =new double[23]
+for ( int i=0; i<temperatira.lenght;i++){
+    
+    temperatura
+
+
+}
+    
+
+}
+
+
+
+
+    
+}
